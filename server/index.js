77cf9adfd79f2e8853e329b1c18b4ed5eb5008fe
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-05-31 22:04:33 -0400

    findingerror

--- server/index.js
+++ server/index.js
@@ -24,71 +24,71 @@ mongoose
   })
   .catch(console.error);
 
-app.get("/todos", async (req, res) => {
-  const todos = await Todo.find();
+// app.get("/todos", async (req, res) => {
+//   const todos = await Todo.find();
 
-  res.json(todos);
-});
+//   res.json(todos);
+// });
 
-app.post("/todo/new", (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-  });
+// app.post("/todo/new", (req, res) => {
+//   const todo = new Todo({
+//     text: req.body.text,
+//   });
 
-  todo.save();
+//   todo.save();
 
-  res.json(todo);
-});
+//   res.json(todo);
+// });
 
-app.delete("/todo/delete/:id", async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id);
+// app.delete("/todo/delete/:id", async (req, res) => {
+//   const result = await Todo.findByIdAndDelete(req.params.id);
 
-  res.json({ result });
-});
+//   res.json({ result });
+// });
 
-app.put("/todo/complete/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
+// app.put("/todo/complete/:id", async (req, res) => {
+//   const todo = await Todo.findById(req.params.id);
 
-  todo.complete = !todo.complete;
+//   todo.complete = !todo.complete;
 
-  todo.save();
+//   todo.save();
 
-  res.json(todo);
-});
+//   res.json(todo);
+// });
 
-app.post("/register", async (req, res) => {
-  const { username, password, email } = req.body;
+// app.post("/register", async (req, res) => {
+//   const { username, password, email } = req.body;
 
-  if (await Account.findOne({ email })) {
-    console.log("username exists");
-    return res.send({ error: "Username exists" });
-  }
+//   if (await Account.findOne({ email })) {
+//     console.log("username exists");
+//     return res.send({ error: "Username exists" });
+//   }
 
-  try {
-    const account = new Account({
-      username: username,
-      email: email,
-      password: password,
-    });
-    account.save();
-    res.send({ status: "Ok" });
-  } catch (error) {
-    res.send({ status: "error" });
-  }
-});
+//   try {
+//     const account = new Account({
+//       username: username,
+//       email: email,
+//       password: password,
+//     });
+//     account.save();
+//     res.send({ status: "Ok" });
+//   } catch (error) {
+//     res.send({ status: "error" });
+//   }
+// });
 
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+// app.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
 
-  const user = await Account.findOne({ username });
-  if (password === user.password) {
-    res.send({ status: "ok" });
-    console.log("Sign in successful");
-  } else {
-    res.send({ error: "Wrong Password" });
-    console.log("Sign in FAILED");
-  }
-});
+//   const user = await Account.findOne({ username });
+//   if (password === user.password) {
+//     res.send({ status: "ok" });
+//     console.log("Sign in successful");
+//   } else {
+//     res.send({ error: "Wrong Password" });
+//     console.log("Sign in FAILED");
+//   }
+// });
 
 // Routes
 app.get("/hi", (req, res) => {
